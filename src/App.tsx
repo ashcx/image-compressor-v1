8f4733b7720168d4f101bb01ee9313b1b2d059b6
ashcx <273097009+ashcx@users.noreply.github.com>
2026-09-16 19:30:45 +0800

    Use PNG app icons

--- src/App.tsx
+++ src/App.tsx
@@ -1753,7 +1753,7 @@ export function App() {
         <div class="brand">
           <img
             class="brand__mark"
-            src={`${import.meta.env.BASE_URL}favicon.svg`}
+            src={`${import.meta.env.BASE_URL}app-icon-128.png`}
             alt=""
             width="42"
             height="42"
